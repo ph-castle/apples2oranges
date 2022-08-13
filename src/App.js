5332d80f0ea44d3ea21e0edee590a5b70e21766b
Heemo Yang <heemo521@gmail.com>
2022-08-12 21:58:58 -0700

    Create lobby component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import * as React from "react";
 import { Container } from "@mui/material";
 import { Header } from "../src/features/nav/Header";
-import { Dashboard } from "./features/Dashboard";
+// import { Dashboard } from "./features/nav/Dashboard";
+import { Lobby } from "./features/nav/Lobby";
 import { StyledEngineProvider } from "@mui/material/styles";
+
 function App() {
   return (
     <StyledEngineProvider injectFirst>
       <div>
         <Header />
         <Container maxWidth="lg">
-            {/* <Dashboard /> */}
+          {/* <Dashboard /> */}
+          <Lobby />
         </Container>
       </div>
     </StyledEngineProvider>
