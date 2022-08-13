ea66364be6dead1d49127cee3b854d64f3aff1c0
crypto-bender <dac3mf@virginia.edu>
2022-08-13 00:37:54 -1000

    set up simple socket based multiplayer server that will allow any client from specified origin to connect to server instance

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-const Server = require('boardgame.io/server').Server;
+const { Server, Origins } = require('boardgame.io/server');
 
-const game = require('../src/components/game').game;
+// const { Gamegame } = require('../src/components/game');
+const { Apples } = require('../src/components/Apples');
 const server = Server({
-  games: [game],
+  games: [Apples],
+  origins: ['http://127.0.0.1:3000'],
 //   credentials/authentification stuff,
 });
 server.run(8000);
