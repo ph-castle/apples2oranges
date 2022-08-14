f7404b0bfe13726478f8962cf1d3116c5038df58
Andy <schwaderer@protonmail.com>
2022-08-14 16:59:47 -0700

    Added annotations, basic invalid move handling, round state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Client } from 'boardgame.io/react';
-import { TicTacToe } from './game/game';
 import { Apples } from './game/Apples';
 
 const App = Client({ game: Apples, numPlayers: 3,});
