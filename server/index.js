264722b130d4d85ed121fcd693d8846b25b03e42
Jessica Chen <jessicachen012@gmail.com>
2022-08-13 17:40:18 -0700

    added body parser

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
 const router = require('./routes/routes.js');
 
 const port = process.env.SERVER_PORT || 3000;
 
 const app = express();
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+// increases data limit for pictures
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: false,
+  })
+);
+app.use(
+  bodyParser.json({
+    limit: '50mb',
+  })
+);
 
 app.use(express.json());
 
