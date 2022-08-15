131061c2c7b0dcccd7dcffe71fa4be592a8164c6
crypto-bender <dac3mf@virginia.edu>
2022-08-15 07:16:39 -1000

    pulled game logic and created super basic board components. changed game server to a different file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import { Client } from 'boardgame.io/react';
-// import { Local } from 'boardgame.io/multiplayer';
 import { SocketIO } from 'boardgame.io/multiplayer';
-// import GameBoard from './components/board';
-// import { Gamegame } from './components/game';
-import { Apples } from './components/Apples';
-
-// var GamegameClient = Client({
-//   board: GameBoard,
-//   game: Gamegame,
-//   numPlayers: 3,
-//   debug: true,
-//   multiplayer: SocketIO({server: 'localhost:8000'}),
-// });
+import { Apples } from './game/Apples';
+import { ApplesBoard } from './game/ApplesBoard';
 
 const ApplesClient = Client({
   game: Apples,
+  board: ApplesBoard,
   numPlayers: 3,
   debug: true,
+  // multiplayer: Local(),
   multiplayer: SocketIO({server: 'localhost:8000'})
 });
 
@@ -27,23 +17,11 @@ const App = () => (
   <div
     style={{ display: 'flex', justifyContent: 'space-around', gap: '1.25rem' }}
   >
-    {/* <GamegameClient playerID="0" />
-    <GamegameClient playerID="1" /> */}
     <ApplesClient playerID="0" />
     <ApplesClient playerID="1" />
     <ApplesClient playerID="2" />
   </div>
 );
-
-// const App = (Client({game: Apples, numPlayers: 2}))
-
-=======
-import { Client } from 'boardgame.io/react';
-import { TicTacToe } from './game/game';
-import { Apples } from './game/Apples';
-
-const App = Client({ game: Apples, numPlayers: 3,});
->>>>>>> 36d24bca196302032f8b2ce4e7d193022d1ce14c
 
 
 export default App;
