ed03e066d3e5e5320228156ed4b25ed226edc241
Heemo Yang <heemo521@gmail.com>
2022-08-15 12:52:43 -0700

    Update Headers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
 import * as React from "react";
-import { Client } from 'boardgame.io/react';
-import { SocketIO } from 'boardgame.io/multiplayer';
-import { Apples } from './game/Apples';
-import { ApplesBoard } from './game/ApplesBoard';
-import { Routes, Route, useParams } from 'react-router-dom';
+import { Client } from "boardgame.io/react";
+import { SocketIO } from "boardgame.io/multiplayer";
+import { Apples } from "./game/Apples";
+import { ApplesBoard } from "./game/ApplesBoard";
+import { Routes, Route, useParams } from "react-router-dom";
 import { Container } from "@mui/material";
-import { Header } from "./features/Header";
+import Header from "./features/Header";
 import Dashboard from "./features/Dashboard";
 import { CreateGame } from "./features/CreateGame";
 import Lobby from "./features/Lobby";
@@ -14,35 +13,42 @@ import { WaitingRoom } from "./features/WaitingRoom";
 import { StyledEngineProvider } from "@mui/material/styles";
 
 function App() {
-  let { matchId } = useParams
+  let { matchId } = useParams;
 
   // generate random matchId (or use create API for authenticated matches)
 
   const ApplesClient = Client({
-   game: Apples,
+    game: Apples,
     board: ApplesBoard,
     numPlayers: 3,
     debug: true,
     // multiplayer: Local(),
-    multiplayer: SocketIO({server: 'localhost:8000'})
+    multiplayer: SocketIO({ server: "localhost:8000" }),
   });
 
-  let applesClients=[<ApplesClient playerID="0" />, <ApplesClient playerID="1" />,  <ApplesClient playerID="2" />];
+  let applesClients = [
+    <ApplesClient playerID="0" />,
+    <ApplesClient playerID="1" />,
+    <ApplesClient playerID="2" />,
+  ];
 
   return (
     <StyledEngineProvider injectFirst>
-      <Header/>
+      <Header />
       <div>
-          <Container maxWidth="lg">
-            <Routes>
-                {/* <Route path="/profile/:username" element={<EditProfile/>}/> */}
-                <Route path="/home" element={<Dashboard/>}/>
-                <Route path="/creategame" element={<CreateGame applesClients={applesClients}/>}/>
-                <Route  path="/joingame" element={<Lobby/>}/>
-                <Route path="/waitingroom" element={<WaitingRoom/>}/>
-                <Route  path="/game/apples/:matchId" element={<Apples/>}/>
-            </Routes>
-          </Container>
+        <Container maxWidth="lg">
+          <Routes>
+            {/* <Route path="/profile/:username" element={<EditProfile/>}/> */}
+            <Route path="/home" element={<Dashboard />} />
+            <Route
+              path="/creategame"
+              element={<CreateGame applesClients={applesClients} />}
+            />
+            <Route path="/joingame" element={<Lobby />} />
+            <Route path="/waitingroom" element={<WaitingRoom />} />
+            <Route path="/game/apples/:matchId" element={<Apples />} />
+          </Routes>
+        </Container>
       </div>
     </StyledEngineProvider>
   );
@@ -50,6 +56,4 @@ function App() {
 
 export default App;
 
-
 // pages: edit profile, create custom cards, logout
-
--- src/features/Header.js
+++ src/features/Header.js
@@ -1,24 +1,31 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
+
 import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import { Outlet } from 'react-router-dom';
 
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+import IconButton from "@mui/material/IconButton";
+import { GiShinyApple, GiOrangeSlice } from "react-icons/gi";
+import {
+  AppBar,
+  Box,
+  Container,
+  Avatar,
+  Button,
+  Toolbar,
+  Tooltip,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@mui/material/";
 
-export const Header = () => {
+const pages = ["Join a Game"];
+// const pages = [];
+const settings = ["Logout"];
+// const settings = [];
+
+const Header = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -39,7 +46,11 @@ export const Header = () => {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Box sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}>
+            <GiShinyApple fontSize="1.5rem" />
+            <GiOrangeSlice fontSize="1.5rem" />
+            {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
+          </Box>
           <Typography
             variant="h6"
             noWrap
@@ -48,14 +59,14 @@ export const Header = () => {
             sx={{
               mr: 2,
               display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
+              fontFamily: "Roboto",
               fontWeight: 700,
-              letterSpacing: ".3rem",
+              // letterSpacing: ".1rem",
               color: "inherit",
               textDecoration: "none",
             }}
           >
-            LOGO
+            Apples to Oranges
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -87,16 +98,20 @@ export const Header = () => {
                 display: { xs: "block", md: "none" },
               }}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
+          <Box sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}>
+            <GiShinyApple fontSize="1.5rem" />
+            <GiOrangeSlice fontSize="1.5rem" />
+          </Box>
           <Typography
-            variant="h5"
+            variant="h7"
             noWrap
             component="a"
             href=""
@@ -104,60 +119,82 @@ export const Header = () => {
               mr: 2,
               display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: "monospace",
+              fontFamily: "Roboto",
               fontWeight: 700,
-              letterSpacing: ".3rem",
+              // letterSpacing: ".3rem",
               color: "inherit",
               textDecoration: "none",
             }}
           >
-            LOGO
+            Apples 2 Oranges
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {settings.map((setting) => (
+            {pages.map((page) => (
               <Button
-                key={setting}
+                key={page}
                 onClick={handleCloseNavMenu}
                 sx={{ my: 2, color: "white", display: "block" }}
               >
-                {setting}
+                {page}
               </Button>
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          {!isLoggedIn ? (
+            <Box sx={{ display: "flex", gap: "1em" }}>
+              <Button sx={{ color: "white", border: "1px solid white" }}>
+                Join
+              </Button>
+              <Button
+                sx={{ backgroundColor: "white", border: "1px solid white" }}
+                onClick={() => setIsLoggedIn(true)}
+              >
+                Login
+              </Button>
+            </Box>
+          ) : (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  {/* Put user's avatar here */}
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography
+                      textAlign="center"
+                      onClick={(e) => {
+                        e.target.textContent === "Logout" &&
+                          setIsLoggedIn(false);
+                      }}
+                    >
+                      {setting}
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          )}
         </Toolbar>
       </Container>
-      <Outlet/>
     </AppBar>
   );
 };
-
+export default Header;
