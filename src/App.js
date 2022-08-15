a106af99517572ea5d219a533cc70f3a0e8fe6b0
Jessica Chen <jessicachen012@gmail.com>
2022-08-15 09:56:26 -0700

    added login to App and user as global variable

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { Container } from "@mui/material";
 import { Header } from "../src/features/nav/Header";
-import { Dashboard } from "./features/Dashboard";
+import { Dashboard } from "./features/nav/Dashboard";
 import { CreateGame } from "./features/CreateGame";
 import { StyledEngineProvider } from "@mui/material/styles";
+import Login from './features/login/Login.jsx';
 
 export default function App() {
+
+  const [user, setUser] = useState({});
+
   return (
     <StyledEngineProvider injectFirst>
       <div>
         <Header />
         <Container maxWidth="lg">
-
+          <Login setUser={setUser}/>
             {/* <Dashboard /> */}
           <CreateGame/>
         </Container>
