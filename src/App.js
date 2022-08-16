57fc00120c9a17b6ef4c4917687c58cd3e4a3527
Jessica Chen <jessicachen012@gmail.com>
2022-08-16 15:00:22 -0700

    added routes for login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Client } from 'boardgame.io/react';
 import { SocketIO } from 'boardgame.io/multiplayer';
 import { Apples } from './game/Apples';
 import { ApplesBoard } from './game/ApplesBoard';
 import { Routes, Route, useParams } from 'react-router-dom';
 import { Container } from "@mui/material";
-import { Header } from "./features/Header";
+import Header from "./features/Header";
 import Dashboard from "./features/Dashboard";
 import { CreateGame } from "./features/CreateGame";
 import Lobby from "./features/Lobby";
 import { WaitingRoom } from "./features/WaitingRoom";
 import { StyledEngineProvider } from "@mui/material/styles";
-import Login from './features/login/Login.jsx';
+import ProfilePage from './features/login/ProfilePage';
+import EditProfile from './features/login/EditProfile';
+import LoginPage from './features/login/LoginPage';
+import CreateUserPage from './features/login/CreateUserPage';
 
-function App() {
+export default function App() {
   // let { matchId } = useParams()
   let matchID = '0';
   // generate random matchId (or use create API for authenticated matches)
@@ -25,6 +28,10 @@ function App() {
     'avatar': null
   });
 
+  useEffect(() => {
+    console.log('updated user: ', user);
+  }, [user]);
+
   const ApplesClient = Client({
    game: Apples,
     board: ApplesBoard,
@@ -34,28 +41,33 @@ function App() {
     multiplayer: SocketIO({server: 'localhost:8000'})
   });
 
-  let applesClients=[<ApplesClient matchID={matchID} playerID="0" />, <ApplesClient matchID={matchID} playerID="1" />,  <ApplesClient matchID={matchID} playerID="2" />];
+  let applesClients=[
+    <ApplesClient matchID={matchID} playerID="0" />,
+    <ApplesClient matchID={matchID} playerID="1" />,
+    <ApplesClient matchID={matchID} playerID="2" />
+  ];
 
   return (
     <StyledEngineProvider injectFirst>
-      <Header/>
+      <Header user={user} setUser={setUser}/>
       <div>
-          <Container maxWidth="lg">
-            <Routes>
-                {/* <Route path="/profile/:username" element={<EditProfile/>}/> */}
-                <Route path="/home" element={<Dashboard/>}/>
-                <Route path="/creategame" element={<CreateGame applesClients={applesClients}/>}/>
-                <Route path="/joingame" element={<Lobby/>}/>
-                <Route path="/waitingroom" element={<WaitingRoom/>}/>
-                <Route path="/game/apples/:matchId" element={<Apples/>}/>
-            </Routes>
-          </Container>
+        <Container maxWidth="lg">
+          <Routes>
+            <Route path="/user/login" element={<LoginPage setUser={setUser}/>}/>
+            <Route path="/user/profile" element={<ProfilePage user={user}/>}/>
+            <Route path="/user/edit" element={<EditProfile user={user} setUser={setUser}/>}/>
+            <Route path="/user/create" element={<CreateUserPage setUser={setUser}/>}/>
+            <Route path="/home" element={<Dashboard/>}/>
+            <Route path="/creategame" element={<CreateGame applesClients={applesClients}/>}/>
+            <Route path="/joingame" element={<Lobby/>}/>
+            <Route path="/waitingroom" element={<WaitingRoom/>}/>
+            <Route path="/game/apples/:matchId" element={<Apples/>}/>
+          </Routes>
+        </Container>
       </div>
     </StyledEngineProvider>
   );
-}
-
-export default App;
+};
 
 
 // pages: edit profile, create custom cards, logout
