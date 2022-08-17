fd9fbcf7a45d0b9d314607b0bf45c29d125db5ca
Jessica Chen <jessicachen012@gmail.com>
2022-08-16 18:12:29 -0700

    passed user to Header

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export default function App() {
 
   return (
     <StyledEngineProvider injectFirst>
-      <Header />
+      <Header user={user} setUser={setUser}/>
       <div>
         <Container maxWidth="lg">
           <Routes>
