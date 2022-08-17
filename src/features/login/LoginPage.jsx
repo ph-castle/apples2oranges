4df6d2bcfb2e0c366834749d98d9497e4fa645c4
Jessica Chen <jessicachen012@gmail.com>
2022-08-16 17:56:36 -0700

    added material UI styling

--- src/features/login/LoginPage.jsx
+++ src/features/login/LoginPage.jsx
@@ -2,12 +2,27 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import './Login.css';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/material/FormControl';
+import FormGroup from '@mui/material/FormGroup';
+import FormHelperText from '@mui/material/FormHelperText';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export default function LoginPage({ setUser }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  // username-password combo is valid
   const [valid, setValid] = useState(true);
-  const [firstTry, setFirstTry] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
+  // disables login button when attempting login
+  const [attemptingLogin, setAttemptingLogin] = useState(false);
   let navigate = useNavigate();
 
   const axiosInstance = axios.create({
@@ -16,6 +31,7 @@ export default function LoginPage({ setUser }) {
 
   const handleLogin = (e) => {
     e.preventDefault();
+    setAttemptingLogin(true);
 
     let options = {
       params: {
@@ -27,16 +43,16 @@ export default function LoginPage({ setUser }) {
       .then((results) => {
         if (results.data.id === undefined) {
           setValid(false);
-          setFirstTry(false);
+          setAttemptingLogin(false);
         } else {
           setValid(true);
-          setFirstTry(false);
           setUsername('');
           setPassword('');
           // updates state of user
           setUser(results.data);
           // switch page to home
           navigate('/home');
+          setAttemptingLogin(false);
         }
       })
       .catch((err) => {
@@ -44,44 +60,80 @@ export default function LoginPage({ setUser }) {
       })
   };
 
-  const createAccount = () => {
-    // switch to createUserPage
-    navigate('/user/create');
-  }
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
 
   return (
-    <div className="Login-page">
-      <h3>Login</h3>
-      <form onSubmit={(e) => handleLogin(e)}>
-        <label>Username: </label>
-        <input
-          type="text"
-          required
-          autoComplete="off"
-          value={username}
-          placeholder="Enter username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br/>
-        <label>Password: </label>
-        <input
-          type="password"
-          required
-          autoComplete="off"
-          value={password}
-          placeholder="Enter password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {!firstTry &&
-          (valid ?
-            <div className="success">Success!</div>
-            : <div className="warning">Incorrect password</div>
-          )
-        }
-        <br/>
-        <button type="submit">Login</button>
+    <Box
+      sx={{
+          width: '50%',
+          margin: 'auto',
+          mt: 4,
+          display: 'block'
+      }}>
+      <Typography variant="h4">Login</Typography>
+      <form noValidate autoComplete="off" onSubmit={(e) => handleLogin(e)}>
+        <FormGroup sx={{ mt: '1rem' }}>
+          <FormControl sx={{ width: '25ch', mt: '1rem' }}>
+            <InputLabel required>Username</InputLabel>
+            <OutlinedInput
+              type="text"
+              value={username}
+              required
+              onChange={(e) => setUsername(e.target.value)}
+              label="Username"
+            />
+          </FormControl>
+          <br/>
+          <FormControl sx={{ width: '25ch', mt: '1rem' }}>
+            <InputLabel required>Password</InputLabel>
+            <OutlinedInput
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              required
+              onChange={(e) =>setPassword(e.target.value)}
+              label="Password"
+              error={!valid}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => {setShowPassword(!showPassword)}}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+            {
+              !valid ?
+              (<FormHelperText>Incorrect password</FormHelperText>)
+              :
+              (<FormHelperText>&nbsp;</FormHelperText>)
+            }
+          </FormControl>
+          <FormControl sx={{ width: '25ch', mt: '1rem' }}>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ p: "sm" }}
+            disabled={attemptingLogin}
+          >
+            Login
+          </Button>
+          </FormControl>
+        </FormGroup>
       </form>
-      <button onClick={() => createAccount()}>Create new account</button>
-    </div>
+      <Button
+        variant="contained"
+        sx={{ p: "sm", width: '26ch', mt: '2rem' }}
+        onClick={(e) => navigate('/user/create')}
+      >
+        Create account
+      </Button>
+    </Box>
   )
 }
