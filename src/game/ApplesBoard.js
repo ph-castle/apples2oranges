5cc10d92a98db5abf2a6f4f77fc650173893b45d
Andy <schwaderer@protonmail.com>
2022-08-18 12:25:50 -0700

    Next Game

--- src/game/ApplesBoard.js
+++ src/game/ApplesBoard.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import JudgeView from "./ApplesBoardComponents/JudgeView";
 import PlayerView from "./ApplesBoardComponents/PlayerView";
+import { useNavigate } from "react-router-dom";
+import { lobbyClient } from '../features/utils/lobbyClient';
 // import PlayPhase from './ApplesBoardComponents/PlayPhase';
 // import DealPhase from './ApplesBoardComponents/DealPhase';
 // import ScoreBoard from './ApplesBoardComponents/ScoreBoard';
@@ -8,6 +10,7 @@ import PlayerView from "./ApplesBoardComponents/PlayerView";
 
 export function ApplesBoard({ ctx, G, moves, playerID }) {
   const [roundTime, setRoundTime] = useState();
+  let navigate = useNavigate();
   //create two different views that will be returned depending on if player is judge or not
 
   //define functions that affect how the board elements are displayed
@@ -15,9 +18,36 @@ export function ApplesBoard({ ctx, G, moves, playerID }) {
   //define board elements on when win condition is met
   let winner = "";
   if (ctx.gameover) {
+    let nextGameParams = {
+      playerID: localStorage.getItem("id"),
+      credentials: localStorage.getItem("credentials")
+      // defaults numPlayers and setupData to previous game
+    };
     winner =
       ctx.gameover.winner !== undefined ? (
-        <div id="winner">Winner: {ctx.gameover.winner}</div>
+        <div id="winner">
+          Winner: {ctx.gameover.winner}
+          <button onClick={() => {
+            lobbyClient
+              .playAgain("Apples2Oranges", localStorage.getItem("matchID"), nextGameParams)
+              .then((nextMatchID) => {
+                console.log("next match id: ", nextMatchID);
+                localStorage.setItem("matchID", nextMatchID);
+                navigate(`/waitingroom/${nextMatchID}`);
+              })
+              .catch((err) => {
+                console.log('Error creating next game: ', err);
+              });
+          }}>
+            Play Again
+          </button>
+          <button onClick={() => {
+            navigate('/home');
+          }}
+          >
+            Back to Home
+          </button>
+        </div>
       ) : null;
   }
   //board elements will change based on phase, stage, and if it is players turn to be judge
