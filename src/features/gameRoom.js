8c379c47f907946810225b7a2a0cc87a479dbd16
Tsering Dingtsa <kesangdingtsa@gmail.com>
2022-08-18 09:56:55 -0700

    newworking main

--- src/features/gameRoom.js
+++ src/features/gameRoom.js
@@ -1,24 +1,24 @@
-// import React from 'react';
-// import { ApplesClient } from './utils/ApplesClient';
+import React from 'react';
+import { ApplesClient } from './utils/ApplesClient';
 
-// export const Apples2Oranges = () => {
+export const Apples2Oranges = () => {
 
-//   // const AppleClient = Client({
-//   //   game: decks),
-//   //   board: ApplesBoard,
-//   //   numPlayers: 3,
-//   //   debug: true,
-//   //   // multiplayer: Local(),
-//   //   playerID: playerID,
-//   //   matchID: matchID,
-//   //   multiplayer: SocketIO({ server: "localhost:8000" }),
-//   // });
+  // const AppleClient = Client({
+  //   game: decks),
+  //   board: ApplesBoard,
+  //   numPlayers: 3,
+  //   debug: true,
+  //   // multiplayer: Local(),
+  //   playerID: playerID,
+  //   matchID: matchID,
+  //   multiplayer: SocketIO({ server: "localhost:8000" }),
+  // });
 
-//   return (
-//     <ApplesClient
-//       matchID={localStorage.getItem("matchID")}
-//       playerID={localStorage.getItem("id")}
-//       credentials={localStorage.getItem("credentials")}
-//     />
-//   )
-// }
+  return (
+    <ApplesClient
+      matchID={localStorage.getItem("matchID")}
+      playerID={localStorage.getItem("id")}
+      credentials={localStorage.getItem("credentials")}
+    />
+  )
+}
