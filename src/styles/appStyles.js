3179c111fc2fa7ed743d0ba2edbf1f90bdb00439
rochun <royce.h.chun@gmail.com>
2022-08-19 14:33:24 -0700

    Add borders to the card input fields

--- src/styles/appStyles.js
+++ src/styles/appStyles.js
@@ -21,6 +21,7 @@ export const StyledContainer = styled(Box)({
     top: '0',
     left: '0',
     width: '100%',
+    zIndex: -1,
     minHeight: '100%',
     padding: '0 2em',
     backgroundColor: 'rgba(0,0,0,0.9)',
